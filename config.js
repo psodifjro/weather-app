f6705865132e108226d8ec683c55d165e4c1b041
psodifjro <padmitrievich@gmail.com>
2025-07-14 18:18:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const YANDEX_API_KEY = '8272aaf2-9941-4bb9-83b4-9d060dad6305';
+const YANDEX_GEOCODER_KEY = '4fade68f-d633-4bf0-81de-6dda0bcea5d6';
 const YANDEX_API_URL = 'https://api.weather.yandex.ru/v2/forecast';
